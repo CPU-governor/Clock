02496c99c5ae428bb2e82709264b5783219ccb32
Okuchukwu Nzeogwu <cpu.governor@gmail.com>
2023-12-30 00:18:14 +0100

    Update Clock.sh

--- Clock.sh
+++ Clock.sh
@@ -1,1 +1,16 @@
+U nano 7.2             script.sh
+echo "==========TIMER=========="
+echo "Set timer in :"
+echo "1. hours"
+echo "2. minutes"
+echo "3. seconds"
+read option
+if ["$option" -eq 1]; then
+    echo "Enter your time in hours"
+    read Time
+for ((i=10; i>=0; i--)); do
+    echo "Timer: $i seconds left"
+    sleep 10
+done
 
+echo "Timer done!"
