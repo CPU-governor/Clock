72611103573b8fefe39fdea5fc0863801224c1c8
CPU Governor <cpu.governor@gmail.com>
2023-12-30 12:10:01 +0100

    updated clock.sh

--- Clock.sh
+++ Clock.sh
@@ -1,31 +1,36 @@
-# Clock
-prompt (T) {
-echo "==========TIMER=========="
-echo "Set timer in :"
-echo "1. hours"
-echo "2. minutes"
-echo "3. seconds"
-read option
-if ["$option" -eq 1]; then
-    echo "Enter your time in hours"
-    read T # T for time
-    T_S=60*60*T # converting time to seconds
-else if ["$option" -eq 2]; then
-    echo "Enter your time in minutes"
-    read T
-    T_s=60*T # Converting time to seconds
-else if ["$option" -eq 3]; then
-    echo "Enter your time in seconds"
-    read T
-    T_s=T
-else 
-    echo "invalid option"
-    }
-    
-    prompt (T)
-for ((i=T; i>=0; i--)); do
-    echo "Timer: $i seconds left"
-    sleep T
-done
+#!/bin/bash
 
-echo "Timer done!"
+prompt() {
+    echo "==========TIMER=========="
+    echo "Set timer in :"
+    echo "1. hours"
+    echo "2. minutes"
+    echo "3. seconds"
+    read -r option
+
+    if [ "$option" -eq 1 ]; then
+        echo "Enter your time in hours"
+        read -r T
+        T_S=$((60 * 60 * T)) # converting time to seconds
+    elif [ "$option" -eq 2 ]; then
+        echo "Enter your time in minutes"
+        read -r T
+        T_S=$((60 * T)) # Converting time to seconds
+    elif [ "$option" -eq 3 ]; then
+        echo "Enter your time in seconds"
+        read -r T
+        T_S="$T"
+    else 
+        echo "Invalid option"
+        return 1
+    fi
+    
+    for ((i=T_S; i>=0; i--)); do
+        echo "Timer: $i seconds left"
+        sleep 1
+    done
+
+    echo "Timer done!"
+}
+
+prompt
