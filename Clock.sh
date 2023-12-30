8d99c1db34648f1930297a2df2a971f4b8cdc7ad
CPU Governor <cpu.governor@gmail.com>
2023-12-30 10:51:31 +0100

    updated script

--- Clock.sh
+++ Clock.sh
@@ -7,7 +7,18 @@ echo "3. seconds"
 read option
 if ["$option" -eq 1]; then
     echo "Enter your time in hours"
-    read Time
+    read T # T for time
+    T_S=60*60*T # converting time to seconds
+else if ["$option" -eq 2]; then
+    echo "Enter your time in minutes"
+    read T
+    T_s=60*T # Converting time to seconds
+else if ["$option" -eq 3]; then
+    echo "Enter your time in seconds"
+    read T
+    T_s=T
+else 
+    echo "invalid option"
 for ((i=10; i>=0; i--)); do
     echo "Timer: $i seconds left"
     sleep 10
